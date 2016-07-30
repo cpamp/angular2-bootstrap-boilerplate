bcbe74199bd91d2b0265d67593421968fbffbe13
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-07-30 11:58:26 -0500

    Add: compile bundles

--- src/scripts/components/app.component.ts
+++ src/scripts/components/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {AboutComponent} from './app.about';
+import {ContactComponent} from './app.contact';
 
 @Component({
     selector: 'app-component',
-    templateUrl: '../../templates/app.component.html'
+    templateUrl: '../../templates/app.component.html',
+    precompile: [AboutComponent, ContactComponent]
 })
 export class AppComponent {
     
