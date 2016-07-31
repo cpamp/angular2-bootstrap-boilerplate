fc158d88e4b4e23d93ce09d034b22c0e0fbd6677
Clint Pamperin <c.pamperin21@gmail.com>
2016-07-31 12:53:39 -0500

    refactor: turn nav component into app component

--- src/scripts/components/app.component.ts
+++ src/scripts/components/app.component.ts
@@ -1,9 +1,20 @@
 import {Component} from '@angular/core';
+import {NavItem} from '../interfaces/navItemInterface';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {HomeComponent} from './app.home';
+import {AboutComponent} from './app.about';
+import {ContactComponent} from './app.contact';
 
 @Component({
     selector: 'app-component',
-    templateUrl: '../../templates/app.component.html'
+    templateUrl: '../../templates/app.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    precompile: [HomeComponent, AboutComponent, ContactComponent]
 })
 export class AppComponent {
-    
+    public navItems: NavItem[] = [
+        {label: 'Home', path: '/'},
+        {label: 'About', path: '/about'},
+        {label: 'Contact', path: '/contact'}
+    ];
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,14 +1,8 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './components/app.component';
 import {appRouterProviders} from './components/app.routes';
-import {NavComponent} from './components/app.navigation';
 
 bootstrap(AppComponent, [
-    NavComponent
-])
-.catch(err => console.error(err));
-
-bootstrap(NavComponent, [
     appRouterProviders
 ])
 .catch(err => console.error(err));
